f451679e4def56aac129d74dd31a402d5031d874
Daniel Segarra <danielsegarra36@gmail.com>
2019-03-19 18:27:42 -0400

    Removed comment

--- server.js
+++ server.js
@@ -22,16 +22,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server is up and running on port: ${PORT}`)
 );
-
-// example RecipeAPI get call
-// unirest
-//   .get(
-//     "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?number=1&tags=vegetarian%2Cdessert"
-//   )
-//   .header(
-//     "X-RapidAPI-Key",
-//     "91ba7cc592mshfa0a27d73d033dcp1848f6jsn074f7f8f1079"
-//   )
-//   .end(function(result) {
-//     console.log(result.status, result.headers, result.body);
-//   });
