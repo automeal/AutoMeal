a92bd69bab26cf9af8f3d3eb31220d34447b3a5a
Daniel Segarra <danielsegarra36@gmail.com>
2019-04-10 16:36:17 -0400

    testing environment set up

---
+++ server/app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const passport = require('passport');
+require('dotenv').config();
+const recipes = require('./routes/api/recipes');
+const users = require('./routes/api/users');
+const auth = require('./routes/api/auth');
+const ingredientSearch = require('./routes/recipeAPI/ingredientSearch');
+
+const app = express();
+const nodeEnv = process.env.NODE_ENV || 'development';
+
+// if (nodeEnv !== 'production') {
+//   require('dotenv').config();
+// }
+
+const db =
+  nodeEnv === 'test'
+    ? process.env.TEST_MONGODB_URI
+    : nodeEnv === 'development'
+    ? process.env.DEV_MONGODB_URI
+    : process.env.PRODUCTION_MONGODB_URI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true, useCreateIndex: true })
+  .then(() => console.log(`MongoDB(${nodeEnv}) connected...`))
+  .catch(err => console.log(err));
+
+// Bodyparser Middleware
+app.use(express.json());
+// Passport Authentication Middleware
+app.use(passport.initialize());
+// Passport Configuration
+require('./config/passport')(passport);
+
+// API to mongoDB
+app.use('/api/recipes', recipes);
+app.use('/api/users', users);
+app.use('/api/auth', auth);
+
+// API to rapidAPI
+app.use('/recipeAPI/ingredientSearch', ingredientSearch);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+module.exports = { app, mongoose };
---
+++ server/tests/functions.test.js
@@ -0,0 +1,61 @@
+const request = require('supertest');
+const { app, mongoose } = require('../app');
+const User = require('../models/User');
+
+beforeAll(() => {
+  User.remove({}, err => {
+    if (err) throw err;
+  });
+});
+
+afterAll(done => {
+  mongoose.disconnect(done);
+});
+
+describe('Login nonexistent user', () => {
+  test('It should respond with 404 (does not exist)', done => {
+    request(app)
+      .post('/api/users/login')
+      .send({
+        email: 'thugLife@mapGang.com',
+        password: 'dora.newiestPassword'
+      })
+      .then(response => {
+        expect(response.statusCode).toBe(404);
+        done();
+      });
+  });
+});
+
+describe('Register user', () => {
+  test('It should respond with 200 (success)', done => {
+    request(app)
+      .post('/api/users/register')
+      .send({
+        full_name: 'daniel segarra',
+        email: 'daniel@email.com',
+        password: 'password',
+        confirm_password: 'password'
+      })
+      .set('Content-Type', 'application/json')
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+});
+
+describe('Login just created user', () => {
+  test('It should respond with 200 (success)', done => {
+    request(app)
+      .post('/api/users/login')
+      .send({
+        email: 'daniel@email.com',
+        password: 'password'
+      })
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+});
