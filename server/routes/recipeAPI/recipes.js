a8232f85126d74939b6b0010a4c228acf67a67f9
Daniel Segarra <danielsegarra36@gmail.com>
2019-05-06 19:36:47 -0400

    the recipe stuff sorry my brain is dead

--- server/routes/recipeAPI/recipes.js
+++ server/routes/recipeAPI/recipes.js
@@ -6,59 +6,66 @@ const router = express.Router();
 
 // NEXT STEP: exclude from allergies and dietaryRestrictions
 // do this by adding these restrictions to the "intolerances" query below
+// @route   GET recipeAPI/recipes
 router.get('/', (req, res) => {
   // const includeIngredients = ['tomato', 'onions'];
   // const excludeIngredients = ['kale', 'cinnamon'];
   // const intolerances = ['eggs', 'nuts'];
-  const { query, includeIngredients, excludeIngredients, intolerances } = req.query;
+  const { query, cuisine, diet, includeIngredients, excludeIngredients, intolerances } = req.query;
+  console.log(req.query);
   unirest
     .get(
       `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com` +
         `/recipes/searchComplex` +
         `?query=${query}` +
-        `&includeIngredients=${includeIngredients.join('%2C+')}` +
-        `&excludeIngredients=${excludeIngredients.join('%2C+')}` +
-        `&intolerances=${intolerances.join('%2C+')}` +
+        `&cuisine=${cuisine}` +
+        `&diet=${diet}` +
+        // `${includeIngredients !== ''}`
+        // ?
+        `&includeIngredients=${includeIngredients}` +
+        // : `` +
+        `&excludeIngredients=${excludeIngredients}` +
+        // `&intolerances=${intolerances}` +
         `&ignorePantry=false` +
         `&type=main+course` +
-        `&number=5` +
-        `&ranking=1` +
-        // other stuff
-        `&minCalories=150` +
-        `&maxCalories=1500` +
-        `&minFat=5` +
-        `&maxFat=100` +
-        `&minProtein=5` +
-        `&maxProtein=100` +
-        `&minCarbs=5` +
-        `&minAlcohol=0` +
-        `&maxAlcohol=1000` +
-        `&minCaffeine=0` +
-        `&maxCaffeine=1000` +
-        `&minCalcium=0` +
-        `&maxCalcium=1000` +
-        `&minCholesterol=0` +
-        `&maxCholesterol=1000` +
-        `&minSaturatedFat=0` +
-        `&maxSaturatedFat=50` +
-        `&minVitaminA=0` +
-        `&maxVitaminA=5000` +
-        `&minVitaminC=0` +
-        `&maxVitaminC=1000` +
-        `&minVitaminD=0` +
-        `&maxVitaminD=1000` +
-        `&minVitaminE=0` +
-        `&maxVitaminE=1000` +
-        `&minFiber=0` +
-        `&maxFiber=1000` +
-        `&minIron=0` +
-        `&maxIron=1000` +
-        `&minPotassium=0` +
-        `&maxPotassium=1000` +
-        `&minSodium=0` +
-        `&maxSodium=1000` +
-        `&minSugar=0` +
-        `&maxSugar=1000`
+        `&number=10`
+      // `&ranking=1` +
+      // other stuff
+      // `&minCalories=150` +
+      // `&maxCalories=1500` +
+      // `&minFat=5` +
+      // `&maxFat=100` +
+      // `&minProtein=5` +
+      // `&maxProtein=100` +
+      // `&minCarbs=5` +
+      // `&minAlcohol=0` +
+      // `&maxAlcohol=1000` +
+      // `&minCaffeine=0` +
+      // `&maxCaffeine=1000` +
+      // `&minCalcium=0` +
+      // `&maxCalcium=1000` +
+      // `&minCholesterol=0` +
+      // `&maxCholesterol=1000` +
+      // `&minSaturatedFat=0` +
+      // `&maxSaturatedFat=50` +
+      // `&minVitaminA=0` +
+      // `&maxVitaminA=5000` +
+      // `&minVitaminC=0` +
+      // `&maxVitaminC=1000` +
+      // `&minVitaminD=0` +
+      // `&maxVitaminD=1000` +
+      // `&minVitaminE=0` +
+      // `&maxVitaminE=1000` +
+      // `&minFiber=0` +
+      // `&maxFiber=1000` +
+      // `&minIron=0` +
+      // `&maxIron=1000` +
+      // `&minPotassium=0` +
+      // `&maxPotassium=1000` +
+      // `&minSodium=0` +
+      // `&maxSodium=1000` +
+      // `&minSugar=0` +
+      // `&maxSugar=1000`
     )
     .header('X-RapidAPI-Host', 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com')
     .header('X-RapidAPI-Key', process.env.RAPID_API_KEY)
